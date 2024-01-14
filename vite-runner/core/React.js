d921db3b86335e9ee8f3e3dfb5b53d1997014b91
小阿熊 <786507017@qq.com>
2024-01-14 14:45:29 +0800

    feat: day2 实现fiber

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -22,24 +22,78 @@ const createTextNode = (text) => {
 };
 
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+}
 
-  Object.keys(el.props).forEach((key) => {
+let nextWorkOfUnit = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== "children") {
-      dom[key] = el.props[key];
+      dom[key] = props[key];
     }
   });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
 }
+
+function initChildren(fiber) {
+  const children = fiber.props.children;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+
+function performWorkOfUnit(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+    fiber.parent.dom.append(dom);
+    updateProps(dom, fiber.props);
+  }
+  initChildren(fiber);
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+  return fiber.parent?.sibling;
+}
+
+requestIdleCallback(workLoop);
 
 const React = {
   render,
